aaa1a0e55361bc301d898c97782446ba97202f54
tp02ga <trevor@craftycodr.com>
2023-04-05 12:52:05 -0400

    Constructors Part 2

--- src/Computer.java
+++ src/Computer.java
@@ -7,14 +7,27 @@ public class Computer {
 	String cpuType;
 	Double cpuSpeed;
 	
-	public Computer() {
+	public Computer () {
 		// This is the Computer class's NO ARGUMENT constructor
-		System.out.println("We are instantiating a new Computer object!");
+		System.out.println("We are instantiating a new Computer object via the NO arg constructor!");
 		ram = 0;
 		diskSpace = 0;
 		diskType = "No Disk Type Set";
 		cpuType = "No CPU Type Set";
 		cpuSpeed = 0.0;
+	}
+	
+	public Computer (Integer ram, 
+			Integer diskSpace, 
+			String diskType, 
+			String cpuType, 
+			Double cpuSpeed) {
+		System.out.println("We are instantiating a new Computer object via the 5 arg constructor!");
+		this.ram = ram;
+		this.diskSpace = diskSpace;
+		this.diskType = diskType;
+		this.cpuType = cpuType;
+		this.cpuSpeed = cpuSpeed;
 	}
 	
 	public String toString() {
--- src/ObjectsExampleApp.java
+++ src/ObjectsExampleApp.java
@@ -8,28 +8,18 @@ public class ObjectsExampleApp {
 		// 2. Variable name
 		// 3. the new keyword with a Constructor
 		
-		Computer coreI5 = new Computer();
-		coreI5.cpuSpeed = 2.9;
-		coreI5.cpuType = "Intel Core i5";
-		coreI5.diskSpace = 500;
-		coreI5.diskType = "SSD";
-		coreI5.ram = 8;
+		Computer coreI5 = new Computer(8, 500, "SSD", "Intel Core i5", 2.9);		
 		System.out.println(coreI5);
 		
-		Computer coreI7 = new Computer();
-		coreI7.cpuSpeed = 3.2;
-		coreI7.cpuType = "Intel Core i7";
-		coreI7.diskSpace = 500;
-		coreI7.diskType = "SSD";
-		coreI7.ram = 16;
+		Computer coreI7 = new Computer(16, 500, "SSD", "Intel Core i7", 3.2);
 		System.out.println(coreI7);
 		
-		Computer amdRyzen9 = new Computer();
-		amdRyzen9.cpuSpeed = 3.6;
-		amdRyzen9.cpuType = "AMD Ryzen 9";
-		amdRyzen9.diskSpace = 5000;
-		amdRyzen9.diskType = "HDD";
-		amdRyzen9.ram = 16;
+		Computer amdRyzen9 = new Computer(16, 5000, "HDD", "AMD Ryzen 9", 3.6);
+//		amdRyzen9.cpuSpeed = 3.6;
+//		amdRyzen9.cpuType = "AMD Ryzen 9";
+//		amdRyzen9.diskSpace = 5000;
+//		amdRyzen9.diskType = "HDD";
+//		amdRyzen9.ram = 16;
 		System.out.println(amdRyzen9);
 		
 		Computer unusedComputer = new Computer();
