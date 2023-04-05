44ea1c124b57619d87bb4c9d8ce99a81c98fbdca
tp02ga <trevor@craftycodr.com>
2023-04-05 09:31:22 -0400

    Added values to properties

--- src/ObjectsExampleApp.java
+++ src/ObjectsExampleApp.java
@@ -9,10 +9,28 @@ public class ObjectsExampleApp {
 		// 3. the new keyword with a Constructor
 		
 		Computer coreI5 = new Computer();
+		coreI5.cpuSpeed = 2.9;
+		coreI5.cpuType = "Intel Core i5";
+		coreI5.diskSpace = 500;
+		coreI5.diskType = "SSD";
+		coreI5.ram = 16;
 		System.out.println(coreI5);
 		
 		Computer coreI7 = new Computer();
+		coreI7.cpuSpeed = 3.2;
+		coreI7.cpuType = "Intel Core i7";
+		coreI7.diskSpace = 500;
+		coreI7.diskType = "SSD";
+		coreI7.ram = 32;
 		System.out.println(coreI7);
+		
+		Computer amdRyzen9 = new Computer();
+		amdRyzen9.cpuSpeed = 3.6;
+		amdRyzen9.cpuType = "AMD Ryzen 9";
+		amdRyzen9.diskSpace = 5000;
+		amdRyzen9.diskType = "HDD";
+		amdRyzen9.ram = 32;
+		System.out.println(amdRyzen9);
 	}
 
 }
