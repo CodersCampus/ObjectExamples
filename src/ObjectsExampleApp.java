3627840c2cf8fdf1c30b1e8aae1b8228afb46068
tp02ga <trevor@craftycodr.com>
2023-04-05 09:07:37 -0400

    Added runnable main method

---
+++ src/ObjectsExampleApp.java
@@ -0,0 +1,18 @@
+
+public class ObjectsExampleApp {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+		// What you need to instantiate (create) an Object
+		// 1. Data type
+		// 2. Variable name
+		// 3. the new keyword with a Constructor
+		
+		Computer coreI5 = new Computer();
+		System.out.println(coreI5);
+		
+		Computer coreI7 = new Computer();
+		System.out.println(coreI7);
+	}
+
+}
